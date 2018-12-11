73342bbbb88b40cb7427c12f6461ded96b62ba45
DevExzh <realexzh@gmail.com>
2018-12-11 22:51:19 +0800

    Minecode Frameless Dialog, part of the main window class system

---
+++ source/Frameless/findandreplacedialog.cpp
@@ -0,0 +1,15 @@
+﻿#include "findandreplacedialog.h"
+#include "ui_findandreplacedialog.h"
+
+FindAndReplaceDialog::FindAndReplaceDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::FindAndReplaceDialog)
+{
+    ui->setupUi(this);
+    this->setWindowFlags(Qt::FramelessWindowHint | Qt::Tool);
+}
+
+FindAndReplaceDialog::~FindAndReplaceDialog()
+{
+    delete ui;
+}
---
+++ source/Frameless/projectbuildoptiondialog.cpp
@@ -0,0 +1,41 @@
+﻿#include "projectbuildoptiondialog.h"
+#include "ui_projectbuildoptiondialog.h"
+
+class MainWindow;
+
+ProjectBuildOptionDialog::ProjectBuildOptionDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ProjectBuildOptionDialog)
+{
+    ui->setupUi(this);
+    this->setWindowFlags(Qt::FramelessWindowHint | Qt::Tool);
+
+    QStandardItem *itemDebug=new QStandardItem("Debug");
+    QStandardItem *itemRelease=new QStandardItem("Release");
+    modelBuildType->appendRow(itemDebug);
+    modelBuildType->appendRow(itemRelease);
+
+    QModelIndex itemDebugindex=itemDebug->index();
+
+    ui->listBuildType->setModel(modelBuildType);
+    ui->listBuildType->setCurrentIndex(itemDebugindex);
+}
+
+ProjectBuildOptionDialog::~ProjectBuildOptionDialog()
+{
+    delete ui;
+}
+
+void ProjectBuildOptionDialog::focusOutEvent(QFocusEvent *event)
+{
+    Q_UNUSED(event);
+    emit formClosing();
+    this->close();
+}
+
+void ProjectBuildOptionDialog::on_listBuildType_activated(const QModelIndex &index)
+{
+    QStandardItem *tmpitem=modelBuildType->itemFromIndex(index);
+    emit buildTypeChanged(tmpitem->text());
+    ui->label_7->setText(tmpitem->text());
+}
