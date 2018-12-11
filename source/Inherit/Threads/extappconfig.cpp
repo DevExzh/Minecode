9cd6bd064df6d3f54f6a6fd817bf9ab0c5871109
DevExzh <realexzh@gmail.com>
2018-12-11 22:53:42 +0800

    Minecode Cofiguration File Writer Thread Class

---
+++ source/Inherit/Threads/extappconfig.cpp
@@ -0,0 +1,20 @@
+﻿#include "extappconfig.h"
+
+extAppConfig::extAppConfig(QObject *parent):
+    QObject(parent)
+
+{
+
+}
+
+void extAppConfig::run()
+{
+    QXmlStreamWriter *settingsWriter=new QXmlStreamWriter;
+    settingsWriter->setAutoFormatting(true);
+    settingsWriter->writeStartDocument();
+    settingsWriter->writeStartElement("MinecodeStudio");
+    settingsWriter->writeAttribute("docAutoSave",convertBoolToString(false));
+    settingsWriter->writeAttribute("docExtensionVisiable",convertBoolToString(false));
+    settingsWriter->writeEndElement();
+    settingsWriter->writeEndDocument();
+}
