c88c5c739c52fc7ea5731a2db2c0cf2bb23de5ae
DevExzh <realexzh@gmail.com>
2018-12-11 22:54:37 +0800

    Unused class, will be deleted in the next version

---
+++ source/Inherit/autohidetabwidget.h
@@ -0,0 +1,17 @@
+﻿#ifndef AUTOHIDETABWIDGET_H
+#define AUTOHIDETABWIDGET_H
+
+#include <QTabWidget>
+#include <QTabBar>
+
+class AutoHideTabWidget : public QTabWidget
+{
+    Q_OBJECT
+public:
+    explicit AutoHideTabWidget(QWidget *parent = 0);
+
+private slots:
+    void onTabBarClicked(int);
+};
+
+#endif // AUTOHIDETABWIDGET_H
---
+++ source/Inherit/exsplashscreen.h
@@ -0,0 +1,23 @@
+﻿#ifndef EXSPLASHSCREEN_H
+#define EXSPLASHSCREEN_H
+
+#include <QSplashScreen>
+#include <QPainter>
+
+class exSplashScreen : public QSplashScreen
+{
+public:
+    exSplashScreen(const QPixmap& pixmap);
+    ~exSplashScreen();
+    virtual void drawContents(QPainter *painter);
+    void showStatusMessage(const QString &message, const QColor &color = Qt::black);
+    void setMessageRect(QRect rect, int alignment = Qt::AlignLeft);
+
+private:
+    QString message;
+    int alignement;
+    QColor color;
+    QRect rect;
+};
+
+#endif // EXSPLASHSCREEN_H
