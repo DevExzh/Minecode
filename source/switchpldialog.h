df59b970ea85579650dc9c2b1e6188b77f5e84a4
DevExzh <realexzh@gmail.com>
2018-12-11 22:57:26 +0800

    Useless class, will be deleted in the next version

---
+++ source/switchpldialog.h
@@ -0,0 +1,29 @@
+﻿#ifndef SWITCHPLDIALOG_H
+#define SWITCHPLDIALOG_H
+
+#if _MSC_VER >= 1600
+#pragma execution_character_set("utf-8")
+#endif
+
+#include <QDialog>
+
+namespace Ui {
+class SwitchPLDialog;
+}
+
+class SwitchPLDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SwitchPLDialog(QWidget *parent = 0);
+    ~SwitchPLDialog();
+
+private slots:
+    void on_searchBox_textChanged(const QString &arg1);
+
+private:
+    Ui::SwitchPLDialog *ui;
+};
+
+#endif // SWITCHPLDIALOG_H
