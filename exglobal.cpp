b13f07b169c91a4d1c81f9d55f244570e2207f52
Exzh_PMGI <realexzh@gmail.com>
2019-01-13 18:57:23 +0800

    Minecode Global Methods

---
+++ exglobal.cpp
@@ -0,0 +1,70 @@
+﻿#include "exglobal.h"
+
+bool MinecodeCore::converseBoolean(bool boolean)
+{
+    if(boolean) return false;
+    else return true;
+}
+
+QDir* MinecodeCore::autoDir(const QString &dir)
+{
+    QDir *operatorDir=new QDir(dir);
+    if(!operatorDir->exists())
+        operatorDir->mkpath(dir);
+    return operatorDir;
+}
+
+QFile* MinecodeCore::autoFile(const QString &file)
+{
+    QFile *operatorFile=new QFile(file);
+    if(!operatorFile->exists())
+    {
+        QFileInfo *info=new QFileInfo(*operatorFile);
+        if(!QDir(info->path()).exists())
+        {
+            QDir(info->path()).mkdir(info->path());
+        }
+
+        if(!operatorFile->open(QIODevice::WriteOnly|QIODevice::Text))
+        {
+            qWarning("Cannot open the file :"+file.toLocal8Bit());
+        }
+        safedel(info);
+    }
+    return operatorFile;
+}
+
+QString MinecodeCore::appendSpaces(int length)
+{
+    QString _result;
+    for(int i=0;i<length;++i)
+    {
+        _result+=QString(" ");
+    }
+    return _result;
+}
+
+bool MinecodeCore::isLetter(const QString &input, CaseSensitive sensitive)
+{
+    bool boolean=false;
+    for(int i=0;i<input.length();++i)
+    {
+        if(sensitive==CaseSensitive::Sensitive)
+        {
+            if(input[i]>="a"&&input[i]<="z")
+            {
+                boolean=true;
+            }
+            else boolean=false;
+        }
+        else if(sensitive==CaseSensitive::Insensitive)
+        {
+            if((input[i]>="a"&&input[i]<="z") || (input[i]>="A"&&input[i]<="Z"))
+            {
+                boolean=true;
+            }
+            else boolean=false;
+        }
+    }
+    return boolean;
+}
---
+++ exglobal.h
@@ -0,0 +1,40 @@
+﻿#ifndef EXGLOBAL_H
+#define EXGLOBAL_H
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <QFile>
+#include <QDir>
+
+namespace MinecodeCore
+{
+template<typename Pointer>
+void safedel(Pointer ptr)
+{
+    delete ptr;
+    ptr=nullptr;
+}
+
+enum class CaseSensitive
+{
+    Sensitive,
+    Insensitive
+};
+
+bool converseBoolean(bool boolean);
+QDir* autoDir(const QString &dir);
+QFile* autoFile(const QString &file);
+QString appendSpaces(int length);
+bool isLetter(const QString &input,CaseSensitive sensitive);
+}
+
+#define MinecodeKeepLiveBegin {std::condition_variable kl_cv;std::mutex kl_mtx;std::unique_lock <std::mutex> kl_lck(kl_mtx);\
+        std::thread kl_thread([&](){kl_mtx.lock(); kl_mtx.unlock();
+#define MinecodeKeepLiveEnd	kl_cv.notify_all();});kl_thread.detach(); \
+        while (kl_cv.wait_for(kl_lck, std::chrono::milliseconds(100)) == std::cv_status::timeout){qApp->processEvents();}}
+
+#define MinecodeBuildDate "(unknown)"
+#define import using namespace
+
+#endif // EXGLOBAL_H
